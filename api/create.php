6951bed0207806913986066d2f90facf86012c39
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-03-29 23:48:10 -0300

    Fix create api

--- api/create.php
+++ api/create.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../config/dbclass.php';
+include_once '../config/db.class.php';
 include_once '../controllers/controller.php';
 
 $dbclass = new Database();
